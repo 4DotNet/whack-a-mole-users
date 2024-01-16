0ea95727b4a6957d7ab50c9ed6fb42d17627d65d
Eduard Keilholz <e.keilholz@4dotnet.nl>
2024-01-16 23:02:45 +0100

    Adding log lines

--- src/Wam.Users/Services/UsersService.cs
+++ src/Wam.Users/Services/UsersService.cs
@@ -22,12 +22,14 @@ public class UsersService(IUsersRepository usersRepository, ILogger<UsersService
 
     public async Task<UserDetailsDto> Get(Guid id, CancellationToken cancellationToken)
     {
+        logger.LogInformation("Getting user {@Id}", id);
         var domainModel = await usersRepository.Get(id, cancellationToken);
         return domainModel.ToDto();
     }
 
     public async Task<UserDetailsDto> Ban(Guid id, byte reasonId, CancellationToken cancellationToken)
     {
+        logger.LogInformation("Banning user {@Id} for reason {@ReasonId}", id, reasonId);
         var reason = ExclusionReason.FromId(reasonId);
         var domainModel = await usersRepository.Get(id, cancellationToken);
         domainModel.Exclude(reason);
